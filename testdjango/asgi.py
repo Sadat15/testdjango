588659a9882ad0bf976efdced52f6c0829b2a573
Sadat15 <sadatmanjur15@gmail.com>
2023-04-26 23:52:48 +0100

    Project setup 2

--- testdjango/asgi.py
+++ testdjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for testpage project.
+ASGI config for testdjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testpage.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testdjango.settings')
 
 application = get_asgi_application()
--- testdjango/urls.py
+++ testdjango/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for testpage project.
+URL configuration for testdjango project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
